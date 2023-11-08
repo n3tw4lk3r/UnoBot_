fa85da2197440d68a25b67f5203e529a6d81d9f8
Roma <76955274+lioiioiioil@users.noreply.github.com>
2023-11-08 15:00:21 +0300

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -51,73 +51,73 @@ def add_player(name, iid):
         player[len(player) - 1].cards.append(take_top_card())
 
 def can_put_card(ind):
-    global player, top_of_deck, pos
-    if ((player[pos].cards[ind].number == top_of_deck.number or player[pos].cards[ind].color == top_of_deck.color)) or (player[pos].cards[ind].number == -1 and player[pos].cards[ind].color == 'universal') or (top_of_deck.number == -1 and top_of_deck.color == 'universal'):
+    global player, top_of_deck, current_position
+    if ((player[current_position].cards[ind].number == top_of_deck.number or player[current_position].cards[ind].color == top_of_deck.color)) or (player[current_position].cards[ind].number == -1 and player[current_position].cards[ind].color == 'universal') or (top_of_deck.number == -1 and top_of_deck.color == 'universal'):
         return True
     return False
 
 def put_card(ind):
-    global player, deck_of_cards, pos, top_of_deck, step
-    top_of_deck = player[pos].cards[ind]
-    player[pos].cards = player[pos].cards[:ind] + player[pos].cards[ind + 1:]
+    global player, deck_of_cards, current_position, top_of_deck, step
+    top_of_deck = player[current_position].cards[ind]
+    player[current_position].cards = player[current_position].cards[:ind] + player[current_position].cards[ind + 1:]
     for i in range(top_of_deck.cards_to_next_player):
-        player[(len(player) + pos + step) % len(player)].cards.append(take_top_card())
+        player[(len(player) + current_position + step) % len(player)].cards.append(take_top_card())
     if top_of_deck.change_of_step == 1 or top_of_deck.change_of_step == -1:
         step *= top_of_deck.change_of_step
-        pos = (len(player) + pos + step) % len(player)
+        current_position = (len(player) + current_position + step) % len(player)
     else:
-        pos = (len(player) + pos + step * top_of_deck.change_of_step) % len(player)
-    #print(pos, step)
+        current_position = (len(player) + current_position + step * top_of_deck.change_of_step) % len(player)
+    #print(current_position, step)
 
 def game():
-    global player, deck_of_cards, pos, top_of_deck, step, player_hasActed, is_playing, player_lastMove
-    is_playing = True
-    pos = 0
+    global player, deck_of_cards, current_position, top_of_deck, step, player_hasActed, the_game_is_running, player_lastMove
+    the_game_is_running = True
+    current_position = 0
     step = 1
     top_of_deck = take_top_card()
 
-    while is_playing:
+    while the_game_is_running:
         markup = telebot.types.ReplyKeyboardRemove()
-        unoBot.bot.send_message(unoBot.CHAT_ID, 'верхняя карта: ' + top_of_deck.name, reply_markup=markup)
-        unoBot.bot.send_sticker(unoBot.CHAT_ID, top_of_deck.stiker)
+        #unoBot.bot.send_message(unoBot.CHAT_ID, 'верхняя карта: ' + top_of_deck.name, reply_markup=markup)
+        unoBot.bot.send_sticker(unoBot.CHAT_ID, top_of_deck.stiker, reply_markup=markup)
         buttons = [types.KeyboardButton("Взять карту")]
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=5, selective=True)
-        for i in range(len(player[pos].cards)):
-            msgg = player[pos].cards[i].name
+        for i in range(len(player[current_position].cards)):
+            msgg = player[current_position].cards[i].name
             buttons.append(types.KeyboardButton(msgg))
         keyboard.add(*buttons)
-        unoBot.bot.send_message(unoBot.CHAT_ID, '@' + player[pos].name +' выбери номер карты, которую кинешь или возьми из колоды', reply_markup=keyboard)
+        unoBot.bot.send_message(unoBot.CHAT_ID, '@' + player[current_position].name +' выбери номер карты, которую кинешь или возьми из колоды', reply_markup=keyboard)
 
         count_move = 0
         while True:
             try:
-                a = player_hasActed[player[pos].name]
+                a = player_hasActed[player[current_position].name]
             except IndexError:
                 return
-            while is_playing and player_hasActed[player[pos].name] == False:
+            while the_game_is_running and player_hasActed[player[current_position].name] == False:
                 pass
-            if is_playing:
-                player_hasActed[player[pos].name] = False
-                num = player_lastMove[player[pos].name]
+            if the_game_is_running:
+                player_hasActed[player[current_position].name] = False
+                num = player_lastMove[player[current_position].name]
                 if num == -1:
                     if count_move == 0:
-                        player[pos].cards.append(take_top_card())
-                        num = len(player[pos].cards) - 1
+                        player[current_position].cards.append(take_top_card())
+                        num = len(player[current_position].cards) - 1
                         count_move += 1
-                        if all(can_put_card(i) == False for i in range(len(player[pos].cards))):
+                        if all(can_put_card(i) == False for i in range(len(player[current_position].cards))):
                             buttons = [unoBot.types.KeyboardButton("Пропуск хода")]
                         else:
                             buttons = [unoBot.types.KeyboardButton("Взять карту")]
                         keyboard = unoBot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=5,  selective=True)
-                        for i in range(len(player[pos].cards)):
-                            msgg = player[pos].cards[i].name
+                        for i in range(len(player[current_position].cards)):
+                            msgg = player[current_position].cards[i].name
                             buttons.append(unoBot.types.KeyboardButton(msgg))
                         keyboard.add(*buttons)
-                        unoBot.bot.send_message(unoBot.CHAT_ID, '@' + player[pos].name +" карта добавлена", reply_markup=keyboard)
+                        unoBot.bot.send_message(unoBot.CHAT_ID, '@' + player[current_position].name +" карта добавлена", reply_markup=keyboard)
                     else:
-                        if all(can_put_card(i) == False for i in range(len(player[pos].cards))):
+                        if all(can_put_card(i) == False for i in range(len(player[current_position].cards))):
                             unoBot.bot.send_message(unoBot.CHAT_ID, 'Лошарик, пропускаешь ход')
-                            pos = (len(player) + pos + step) % len(player)
+                            current_position = (len(player) + current_position + step) % len(player)
                             break
                         else:
                             unoBot.bot.send_message(unoBot.CHAT_ID, 'хватит тырить карты из колоды')
@@ -128,19 +128,22 @@ def game():
                     else:
                         unoBot.bot.send_message(unoBot.CHAT_ID, 'Дурачок попробуй ещё')
         if any(len(player[i].cards) == 0 for i in range(len(player))):
-            is_playing = False
+            for i in range(len(player)):
+                if len(player[i].cards) == 0:
+                    unoBot.bot.send_message(unoBot.CHAT_ID, f'ПОЗДРАВЛЯЕМ С ПОБЕДОЙ {player[i].name}')
+            the_game_is_running = False
     markup = telebot.types.ReplyKeyboardRemove()
     unoBot.bot.send_message(unoBot.CHAT_ID, 'Игра закончилась ну блииииин(((09((09(((', reply_markup=markup)
     clear_fields()
 
 def clear_fields():
-    global player, player_hasActed, player_lastMove, deck_of_cards, pos, is_playing
+    global player, player_hasActed, player_lastMove, deck_of_cards, current_position, the_game_is_running
     player = []
     player_hasActed = {}
     player_lastMove = {}
     deck_of_cards = []
-    pos = 0
-    is_playing = False
+    current_position = 0
+    the_game_is_running = False
     #unoBot.bot.send_message(unoBot.CHAT_ID, 'все поля очищены')
 
 file = open("UNO cards.txt", 'r')
@@ -170,5 +173,5 @@ player = []
 player_hasActed = {}
 player_lastMove = {}
 deck_of_cards = []
-pos = 0
-is_playing = False
+current_position = 0
+the_game_is_running = False
