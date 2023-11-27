3d7978fed40322bbdb088d0243c0b79a18f157a0
n3tw4lk3r <75166974+PavelKhod@users.noreply.github.com>
2023-11-27 17:56:48 +0300

    Update sharedVars.py

--- src/sharedVars.py
+++ src/sharedVars.py
@@ -3,8 +3,6 @@ from threading import Thread
 
 from queue_stream import *
 
-bot = telebot.TeleBot('6872862815:AAEDh0fdb15g8XCjghcW4RIJlLOnsEG_i6M')
-
 threadsByChatId = {}
 st = stream()
 q = Thread(target= st.run)
